b7ec5d1f626240338f685eaf40e9c1a119661caa
Kaviyarasan <kaviyarasanandi@gmail.com>
2026-05-20 18:45:40 +0530

    itertaive check

--- backend/main.py
+++ backend/main.py
@@ -148,6 +148,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         "https://qlik-frontend.onrender.com",
+        "https://alteryx-ai-v1.onrender.com",
         "https://qlik-sense-cloud.onrender.com",
         "https://qlikai-ld54.onrender.com",
         "https://qlikaiv2-web.onrender.com",
